7913b4e1bc60ad8ceaaa49f5f72852cd47a385c2
HOUEKPETODJI Honoré <homahugnon@gmail.com>
2025-05-25 14:18:19 +0200

    template example

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 
 import './App.css'
 import Home from "./pages/home.tsx";
+import {TextParallaxContentExample} from "./pages/TextParallaxContentExample.tsx";
 
 const App= ()=> {
 
   return (
       <div className="scroll-smooth">
-        <Home></Home>
+        <TextParallaxContentExample></TextParallaxContentExample>
       </div>
     );
 }
