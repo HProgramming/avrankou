31c17b3fe1ac2f34fd9b343e27829d18f6ec1550
HOUEKPETODJI Honoré <homahugnon@gmail.com>
2025-05-28 12:24:44 +0200

    fix #8

---
+++ src/components/touristic_atraction.tsx
@@ -0,0 +1,50 @@
+const attractions = [
+    {
+        name: "Le Monument aux Dévoués",
+        imageUrl: "/l-esplanade-aux-devoues.jpg",
+    },
+    {
+        name: "L'esplanade de l'Amazone",
+        imageUrl: "/l-esplanade-de-l-amazone.jpg",
+    },
+    {
+        name: "La statue de Bio Guera",
+        imageUrl: "/la-statue-bio-guera.jpg",
+    },
+    {
+        name: "Place des Martyrs",
+        imageUrl: "/place-des-martyrs.jpg",
+    },
+    {
+        name: "La route des pêches",
+        imageUrl: "/route-des-peches.jpg",
+    },
+    {
+        name: "Le Jardin de Mathieu",
+        imageUrl: "/le-jardin-de-mathieu.png",
+    }
+];
+
+const TouristicAtraction = () => {
+    return (
+        <div className={`bg-[#eeeeee] w-full `}>
+            <h1
+                className="heading-primary mb-8 text-primary text-center pt-44 ">
+                Attractions touristiques d'Avrankou</h1>
+
+            <div className={`grid md:grid-cols-3 gap-4 bg-white container mx-auto p-10 rounded-md`}>
+                {attractions.map((attraction, index) =>
+                    <div
+                        key={index}
+                        style={{backgroundImage: `url(${attraction.imageUrl})`}}
+                        className={` h-[400px] w-96 bg-cover bg-blend-overlay rounded-md bg-black/15 bg-center text-white text-center flex justify-center items-center`}
+                    >
+                            <h5 className={`text-2xl font-semibold text-white`}>{attraction.name}</h5>
+
+                    </div>)
+                }
+            </div>
+        </div>
+    )
+}
+export default TouristicAtraction;
