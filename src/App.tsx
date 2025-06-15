9287d3387a2780018fbdfff99ae3699cb2cf67e4
Oluwatoyin D. FALY <46930972+oluwatoyinfaly@users.noreply.github.com>
2025-06-15 16:35:16 +0100

    reorganize app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 
 import './App.css'
+import FlagLine from './components/FlagLine.tsx';
+import Footer from './components/footer.tsx';
+import Navbar from './components/navbar.tsx';
 import Home from "./pages/home.tsx";
 
 const App= ()=> {
 
   return (
-      <div className="scroll-smooth">
-        <Home></Home>
+      <div>
+        <Navbar />
+        <div className="scroll-smooth">
+          <Home />
+        </div>
+        <Footer />
+        <FlagLine />
       </div>
     );
 }
