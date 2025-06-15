abcbb72dddcd16fedbecdd6b85f1e2ef77008413
Oluwatoyin D. FALY <46930972+oluwatoyinfaly@users.noreply.github.com>
2025-06-15 16:35:00 +0100

    add flagline component

---
+++ src/components/FlagLine.tsx
@@ -0,0 +1,9 @@
+export default function FlagLine() {
+    return (
+        <div className="flex h-1">
+            <div className="flex-1 bg-green-500" />
+            <div className="flex-1 bg-yellow-400" />
+            <div className="flex-1 bg-red-500" />
+        </div>
+    )
+}
