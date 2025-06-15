ca289eb52933a43020a9739c08603222c84b1c0c
Oluwatoyin D. FALY <46930972+oluwatoyinfaly@users.noreply.github.com>
2025-06-15 16:36:40 +0100

    remove navbar and footer from home component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,9 @@
-import Footer from "../components/footer.tsx";
-import Navbar from "../components/navbar.tsx";
 import Discover from "./discover.tsx";
 
 const Home = ()=> {
     return (
         <div className={`bg-white`}>
-            <Navbar />
-            <Discover></Discover>
-            <Footer />
+            <Discover />
         </div>
     );
 }
