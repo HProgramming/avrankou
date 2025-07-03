f6d0b7fde457ee0fbc75ddb2a694d76ef52ee444
HOUEKPETODJI Honoré <homahugnon@gmail.com>
2025-07-03 11:46:14 +0200

    mise a jour de navbar historique et economie #15

--- src/components/economy.tsx
+++ src/components/economy.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import {useRef} from 'react';
 import useIntersectionObserver from '../hooks/useIntersectionObserver';
 
 const Economy = () => {
@@ -17,7 +17,8 @@ const Economy = () => {
                     className={`animate-scale-in animate-delay-300`}
                 >
                     <div className={`w-full rounded-4xl`}>
-                        <img src={`https://cotonou.mairie.bj/public/medias/economie-cotonou-1688213145.png`} className={``}
+                        <img src={`https://cotonou.mairie.bj/public/medias/economie-cotonou-1688213145.png`}
+                             className={``}
                              alt={`Fresque mural`}/>
                     </div>
                 </div>
@@ -26,12 +27,13 @@ const Economy = () => {
                     className={`animate-scroll animate-delay-300`}
                 >
                     <h1 className="heading-primary mb-8 text-primary">Situation économique</h1>
-                    <p className={`text-lg font-medium leading-9 mb-8`}>Les activités économiques exercées dans la ville de
-                        Cotonou relèvent surtout du secteur tertiaire,
-                        tel que le commerce et les services, qui sont appuyées par quelques industries manufacturières.
-                        Quant aux activités agricoles, elles n'occupent qu'une petite frange de la population qui pratique
-                        le maraîchage, soit 80% des actifs agricoles. L'artisanat et le commerce constituent la base de
-                        l'économie locale de la municipalité, du fait de la présence de grands opérateurs économiques.</p>
+                    <p className={`text-lg font-medium leading-9 mb-8`}>
+                        Avrankou se distingue par la diversité de ses activités : élevage, pêche, pisciculture,
+                        artisanat, commerce, et surtout agriculture, qui occupe 60 % de la population. Le palmier à
+                        huile, produit phare, a contribué au rayonnement de la commune, notamment avec l'installation
+                        d'une huilerie dès 1947. Grâce à sa proximité avec le Nigéria et Porto-Novo, Avrankou bénéficie
+                        également d’un fort potentiel économique.
+                    </p>
                 </div>
             </div>
         </div>
